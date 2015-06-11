bdbabbe18b3e90ce80e3714c98d366747c054e9c
gustiazis <gooazs@gmail.com>
2015-06-11 17:02:33 +0700

    Sort JS According to Window Events

--- scripts/demo.js
+++ scripts/demo.js
@@ -1,70 +1,3 @@
-/* Selectmenu Extend - JQuery Mobile */
-(function($) {
-	function pageIsSelectmenuDialog(page) {
-		var isDialog = false;
-		var id = page && page.attr('id');
-
-		$('.filterable-select').each(function() {
-			if ($(this).attr('id') + '-dialog' === id) {
-				isDialog = true;
-				return false;
-			}
-		});
-
-		return isDialog;
-	}
-
-	$.mobile.document.on('selectmenucreate', '.filterable-select', function(event) {
-		var input;
-		var selectmenu = $(event.target);
-		var list = $('#' + selectmenu.attr('id') + '-menu');
-		var form = list.jqmData('filter-form');
-
-		if (!form) {
-			input = $('<input data-type="search"></input>');
-			form = $('<form></form>').append(input);
-
-			input.textinput();
-
-			list.before(form).jqmData('filter-form', form);
-			form.jqmData('listview', list);
-		}
-
-		selectmenu.filterable({ input: input, children: '> option[value]' }).on('filterablefilter', function() {
-			selectmenu.selectmenu('refresh');
-		});
-
-	}).on('pagecontainerbeforeshow', function(event, data) {
-		var listview;
-		var form;
-
-		if (!pageIsSelectmenuDialog(data.toPage)) {
-			return;
-		}
-
-		listview = data.toPage.find('ul');
-		form = listview.jqmData('filter-form');
-
-		data.toPage.jqmData('listview', listview);
-
-		listview.before(form);
-
-	}).on('pagecontainerhide', function(event, data) {
-		var listview;
-		var form;
-
-		if (!pageIsSelectmenuDialog(data.toPage)) {
-			return;
-		}
-
-		listview = data.prevPage.jqmData('listview');
-		form = listview.jqmData('filter-form');
-
-		listview.before(form);
-	});
-
-})(jQuery);
-
 /* Datepicker Extend - JQuery UI and Mobile */
 /* http://cdn.rawgit.com/arschmitz/jquery-mobile-datepicker-wrapper/v0.1.1/jquery.mobile.datepicker.js */
 (function($, undefined) {
@@ -192,6 +125,73 @@
 			.find('.ui-datepicker-calendar a.ui-state-active').addClass('ui-btn-active').end()
 			.find('.ui-datepicker-calendar a.ui-state-highlight').addClass('ui-btn-up-a').end().find('.ui-state-disabled').css('opacity', '1');
 		}
+	});
+
+})(jQuery);
+
+/* Selectmenu Extend - JQuery Mobile */
+(function($) {
+	function pageIsSelectmenuDialog(page) {
+		var isDialog = false;
+		var id = page && page.attr('id');
+
+		$('.filterable-select').each(function() {
+			if ($(this).attr('id') + '-dialog' === id) {
+				isDialog = true;
+				return false;
+			}
+		});
+
+		return isDialog;
+	}
+
+	$.mobile.document.on('selectmenucreate', '.filterable-select', function(event) {
+		var input;
+		var selectmenu = $(event.target);
+		var list = $('#' + selectmenu.attr('id') + '-menu');
+		var form = list.jqmData('filter-form');
+
+		if (!form) {
+			input = $('<input data-type="search"></input>');
+			form = $('<form></form>').append(input);
+
+			input.textinput();
+
+			list.before(form).jqmData('filter-form', form);
+			form.jqmData('listview', list);
+		}
+
+		selectmenu.filterable({ input: input, children: '> option[value]' }).on('filterablefilter', function() {
+			selectmenu.selectmenu('refresh');
+		});
+
+	}).on('pagecontainerbeforeshow', function(event, data) {
+		var listview;
+		var form;
+
+		if (!pageIsSelectmenuDialog(data.toPage)) {
+			return;
+		}
+
+		listview = data.toPage.find('ul');
+		form = listview.jqmData('filter-form');
+
+		data.toPage.jqmData('listview', listview);
+
+		listview.before(form);
+
+	}).on('pagecontainerhide', function(event, data) {
+		var listview;
+		var form;
+
+		if (!pageIsSelectmenuDialog(data.toPage)) {
+			return;
+		}
+
+		listview = data.prevPage.jqmData('listview');
+		form = listview.jqmData('filter-form');
+
+		listview.before(form);
 	});
 
 })(jQuery);
